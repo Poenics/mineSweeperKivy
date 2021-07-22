769e3838056d61239350b6c7bb848f6ee4e5bdc4
Terra_Creeper <dkrklksk@gmail.com>
2021-07-22 16:23:26 +0200

    Refactored Code and added Main Menu

--- bomb.py
+++ bomb.py
@@ -1,75 +1,142 @@
+from kivy.core import text
 import numpy as np
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
-
-# w = int(input("Width: "))
-# h = int(input("Height: "))
-w = 5
-h = 5
-
-field = np.zeros((w, h), dtype=bool)
+from kivy.uix.popup import Popup
+import random
 
 
-def setBomb(y, x):
-    field[y][x]=1
-
-setBomb(0, 3)
-setBomb(1, 2)
-
-
-
-class MyGrid(GridLayout):
-    def __init__(self, **kwargs):
-        super(MyGrid, self).__init__(**kwargs)
-        self.cols = w
-        for y in range(h):
-            for x in range(w):
-        # button:
-                self.submit = Button(text=str(y)+" "+str(x))
-                self.submit.bind(on_press=self.pressed)
-                self.add_widget(self.submit)
-
+class Cell(Button):
+    """Minesweeper Cell"""
+    def __init__(self, bomb, index, board, **kwargs):
+        """Minesweeper Cell init"""
+        super(Cell, self).__init__(**kwargs)
+        
+        #Sets bomb-indicator and xy-position
+        self.is_bomb = bomb
+        self.xpos = index[0]
+        self.ypos = index[1]
+        self.board = board
+        self.text = f"{self.xpos} {self.ypos}"
+        self.bind(on_release = self.pressed)
     
     def pressed(self, instance):
-        ind = ("%s" % instance.text).split(" ")
-        print("Index is: %s "% instance.text + "Bomb? " + str(field[int(ind[0])][int(ind[1])]))
-        around(int(ind[0]), int(ind[1]))
+        """Callback for the Cell"""
+        ind = (self.xpos, self.ypos)
+        print("Index is: %s "% instance.text + "Bomb? " + str(self.is_bomb))
+        self.around(int(ind[0]), int(ind[1]))
+
+    def around(self, x, y):
+        """Gets Surrounding Cell Values and counts Bombs"""
+        temp = [[],[],[]]
+        width = self.board.cols -1 
+        height = self.board.rows -1
+
+        temp[0].append(self.getval(y-1,x-1) if x>0 and y>0 else None)
+        print(temp[0][0])
+        temp[0].append(self.getval(y-1, x) if y>0 else None)
+        temp[0].append(self.getval(y-1, x+1) if x<width and y>0 else None)
+
+        temp[1].append(self.getval(y,x-1) if x>0 else None)
+        temp[1].append(self.is_bomb)
+        temp[1].append(self.getval(y,x+1) if x<width else None)
         
+        temp[2].append(self.getval(y+1,x-1) if x > 0 and y<height else None)
+        temp[2].append(self.getval(y+1,x) if y<height else None)
+        temp[2].append(self.getval(y+1,x+1) if x<width and y<height else None)
+
+        temp = np.array(temp)
+        print(temp)
+        print("Amount of Bombs near me: " + str(self.fieldsum(temp)-self.is_bomb))
     
+    def getval(self, y,x):
+        """Gets field value"""
+        return self.board.field[y][x]
+
+    def fieldsum(self, field):
+        """Sums the amount of bombs in a given field"""
+        num = 0
+        for i in field:
+            for j in i:
+                if j:
+                    num += 1
+        return num
+
+class GameBoard(GridLayout):
+    """Holds all Cells"""
+    def __init__(self, x, y, bombs, **kwargs):
+        """Game Board Init"""
+        super(GameBoard, self).__init__(**kwargs)
+        self.rows = y
+        self.cols = x
+        self.field = np.zeros((x, y), dtype=bool)
+
+        # Adjusts bomb count if ist less than zero or more than the amount of cells
+        if bombs < 0:
+            bombs = 0
+        if bombs > x*y:
+            bombs = x*y-1
+
+        # if bomb count is more than half of the cells, randomly removes bombs until bomb count is reached
+        # else randomly adds bombs until bomb count is reached
+        if bombs > 1/2 * x*y:
+            self.field = 1- self.field
+            reverse = False
+        else:
+            reverse = True
+        
+        while self.field.sum() != bombs:
+            self.field[random.randint(0,y-1)][random.randint(0,x-1)] = reverse
+
+        # adds cells
+        for i in range(y):
+            for j in range(x):
+                self.add_widget(Cell(self.field[j,i], (j,i), self))
+
+    
+        
+
+class MainMenu(BoxLayout):
+    """Main Menu for the Minesweeper App"""
+    def __init__(self, **kwargs):
+        """Constructing Main Menu"""
+        super().__init__(**kwargs)
+        self.orientation = "vertical"
+
+        # Adding Widgets
+        xinput = TextInput(hint_text = "Insert Board Width", text = "10")
+        yinput = TextInput(hint_text = "Insert Board height", text = "10")
+        binput = TextInput(hint_text = "Insert Bomb Count", text = "20")
+        startbutton = Button(text = "start")
+        startbutton.bind(on_release = lambda x: self.startGame(int(xinput.text), int(yinput.text), int(binput.text)))
+
+        self.add_widget(xinput)
+        self.add_widget(yinput)
+        self.add_widget(binput)
+        self.add_widget(startbutton)
+    
+    def startGame(self, x, y, bombs):
+        """Opens Popup and starts Minesweeper Game"""
+        layout = BoxLayout(orientation = "vertical")
+        game = Popup(title = "Game", content = layout)
+
+        back = Button(text = "Back", size_hint_y = 0.1)
+        back.bind(on_release = game.dismiss)
+
+        layout.add_widget(GameBoard(x, y, bombs))
+        layout.add_widget(back)
+
+        game.open()
 
 
 class MyApp(App):
     def build(self):
-        return MyGrid()
-
-def around(y, x):
-    # i hate this but i really have to sleep now (its past 1am)
-    if y != 0:
-        top = y-1
-    else:
-        top = y
-    if y != h:
-        bottom = y+2
-    else:
-        bottom = y
-    if x != 0:
-        strange = x-1
-    else:
-        strange = x
-    if x != w:
-        charm = x+2
-    else:
-        charm = x
-    # you have no idea how much i hate this
-    
-    temp = field[top:bottom, strange:charm]
-    print(temp)
-    print("Amount of Bombs near me: " + str(temp.sum()))
-
+        return MainMenu()
 
 if __name__ == "__main__":
     MyApp().run()
