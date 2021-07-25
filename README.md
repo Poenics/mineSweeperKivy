21711dfee98db8ba720806b5815b21024de5f8c5
Terra_Creeper <dkrklksk@gmail.com>
2021-07-25 16:21:12 +0200

    Added ability to flag cells and added showing all cells when a bomb is revealed

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # mineSweeperKivy
 Making Minesweeper for android using Kivy
+
+
+Licensed Images Used:
+- flag & bomb icon: https://fontello.com/
+- shovel icon: https://www.svgrepo.com/svg/82675/shovel
--- bomb.py
+++ bomb.py
@@ -1,3 +1,4 @@
+from os import stat
 import random
 import numpy as np
 import kivy
@@ -9,16 +10,17 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
 from kivy.uix.textinput import TextInput
+from kivy.uix.togglebutton import ToggleButton
 
 """
 TODO
-- Distinguish between flagging and revealing cell (double dropdown/toggle button)
-- Reveal Cell when clicked
-- Flag Cell when clicked
-- Display surrounding Bomb Count and change background color
-- Display Bomb Icon
-- Display Flag Icon
-- Game over when revealing Bomb
+- DONE: Distinguish between flagging and revealing cell (double dropdown/toggle button)
+- DONE: Reveal Cell when clicked
+- DONE: Flag Cell when clicked
+- DONE: Display surrounding Bomb Count and change background color
+- DONE: Display Bomb Icon
+- DONE: Display Flag Icon
+- WIP: Game over when revealing Bomb
 - Reveal surrounding Cells when revealing clear Cell
 - Win once only all bombs are flagged
 - Regenerate Board on Game End
@@ -26,6 +28,7 @@ TODO
 - Bild Lizensen?
 """
 
+shovel_icon = ""
 flag_icon = ""
 flag_color = (1, 0, 0, 1)
 bomb_icon = ""
@@ -39,22 +42,50 @@ six_color = (0, 0.5, 0.5, 1)
 seven_color = (0, 0, 0, 1)
 eight_color = (0.5, 0.5, 0.5, 1)
 
+class ToolBar(BoxLayout):
+    """Tool Bar for switching cell modes or displaying information"""
+    def __init__(self, **kwargs):
+        """Init for ToolBar"""
+        super().__init__(**kwargs)
+        self.orientation = "horizontal"
+        self.reveal = ToggleButton(text = shovel_icon, group = "tool", state = "down")
+        self.reveal.font_name = "celltext.ttf"
+        self.reveal.background_normal = "normal.png"
+        self.reveal.background_down = "down.png"
+        self.reveal.color = (0,0,0,1)
+        self.add_widget(self.reveal)
+        self.flag = ToggleButton(text = flag_icon, group = "tool")
+        self.flag.font_name = "celltext.ttf"
+        self.flag.background_normal = "normal.png"
+        self.flag.background_down = "down.png"
+        self.flag.color = (0,0,0,1)
+        self.add_widget(self.flag)
+        self.size_hint_y = 0.05
+
+    def isFlaggingEnabled(self) -> bool:
+        """Returns if the Flag Button is enabled or not"""
+        if self.reveal.state == "down":
+            return False
+        elif self.flag.state == "down":
+            return True
 
 class GameBoard(GridLayout):
     """Holds all Cells"""
-    def __init__(self, width : int, height : int, bomb_count : int, **kwargs):
+    def __init__(self, width : int, height : int, bomb_count : int, tool_bar: ToolBar, **kwargs):
         """Game Board Init"""
         super(GameBoard, self).__init__(**kwargs)
+        self.tool_bar = tool_bar
         self.rows = height
         self.cols = width
         self.field = np.zeros((height,width), dtype=bool)
-        print(self.field)
+        self.first_reveal = True
 
         # Adjusts bomb count if ist less than zero or more than the amount of cells
         if bomb_count < 0:
             bomb_count = 0
         if bomb_count > width*height:
             bomb_count = width*height
+        self.bomb_count = bomb_count
 
         # if bomb count is more than half of the cells, randomly removes bombs until bomb count is reached
         # else randomly adds bombs until bomb count is reached
@@ -67,12 +98,21 @@ class GameBoard(GridLayout):
         while self.field.sum() != bomb_count:
             self.field[random.randint(0,height-1)][random.randint(0,width-1)] = cell_value
 
+        self.field = list(map(lambda x: list(x), self.field))
         # adds cells
         for i in range(height):
             for j in range(width):
-                self.add_widget(Cell(self.field[i,j], (j,i), self))
+                cell = Cell(self.field[i][j], (j,i), self)
+                self.add_widget(cell)
+                self.field[i][j] = cell
+
         for i in self.children:
             i.conceal()
+    def lose(self):
+        for i in self.children:
+            i.disabled = True
+            i.updateDisplay()
+    
 
     
 class Cell(Button):
@@ -83,6 +123,7 @@ class Cell(Button):
         
         # Sets bomb-indicator and xy-position
         self.is_bomb = is_bomb
+        self.is_flagged = False
         self.x_index = index[0]
         self.y_index = index[1]
         self.game_board = board
@@ -95,14 +136,32 @@ class Cell(Button):
         else:
             self.display_flag()
         self.background_down = "down.png"
+        self.background_disabled_down = "down.png"
+        self.background_disabled_normal = "down.png"
 
     
     def pressed(self, instance: Button):
         """Callback for the Cell"""
-        index = (self.x_index, self.y_index)
-        print("Index is: %s "% str(index) + "Bomb? " + str(self.is_bomb))
-        self.around(int(index[0]), int(index[1]))
-        self.updateDisplay()
+        # index = (self.x_index, self.y_index)
+        # print("Index is: %s "% str(index) + "Bomb? " + str(self.is_bomb))
+        # self.around(int(index[0]), int(index[1]))
+        flagging_enabled = self.game_board.tool_bar.isFlaggingEnabled()
+        if self.game_board.first_reveal and not flagging_enabled:
+            self.game_board.bomb_count -= self.is_bomb
+            self.is_bomb = False
+            self.game_board.first_reveal = False
+        
+        if not flagging_enabled and self.is_bomb:
+            self.game_board.lose()
+        elif not flagging_enabled and not self.is_flagged:
+            self.updateDisplay()
+        elif flagging_enabled and not self.is_flagged:
+            self.is_flagged = True
+            self.display_flag()
+        elif flagging_enabled:
+            self.conceal()
+            self.is_flagged = False
+        
 
     def around(self, x_index: int, y_index: int):
         """Gets Surrounding Cell Values and counts Bombs"""
@@ -139,7 +198,7 @@ class Cell(Button):
         temp_list[0].append(self.getval(y_index - 1, x_index + 1) if x_index < width and y_index > 0 else None)
 
         temp_list[1].append(self.getval(y_index, x_index - 1) if x_index > 0 else None)
-        temp_list[1].append(self.is_bomb)
+        temp_list[1].append(self)
         temp_list[1].append(self.getval(y_index, x_index + 1) if x_index < width else None)
         
         temp_list[2].append(self.getval(y_index + 1, x_index - 1) if x_index > 0 and y_index < height else None)
@@ -157,13 +216,28 @@ class Cell(Button):
         """Gets field value"""
         return self.game_board.field[y_index][x_index]
 
-    def fieldsum(self, field : np.array):
+    @staticmethod
+    def fieldsum(field):
         """Sums the amount of bombs in a given field"""
         amount = 0
         for i in field:
             for j in i:
-                if j:
-                    amount += 1
+                if j is None:
+                    continue
+                else:
+                    amount += j.is_bomb
+        return amount
+
+    @staticmethod
+    def flagsum(field):
+        """Sums the amount of flagged cells in a given field"""
+        amount = 0
+        for i in field:
+            for j in i:
+                if j is None:
+                    continue
+                else:
+                    amount += j.is_flagged
         return amount
 
     def display_flag(self):
@@ -171,17 +245,20 @@ class Cell(Button):
         self.font_name = "celltext.ttf"
         self.text = flag_icon
         self.color = flag_color
-        self.background_normal = "down.png"
+        self.background_normal = "normal.png"
 
     def display_bomb(self):
         """Displays the Bomb"""
+        self.disabled = True
         self.font_name = "celltext.ttf"
         self.text = bomb_icon
-        self.color = bomb_color
-        self.background_normal = "down.png"
+        self.disabled_color = bomb_color
+        self.background_disabled_normal = "normal.png"
+        self.background_normal = "normal.png"
 
     def display_zero(self):
         """Displays the Zero"""
+        self.disabled = True
         self.font_name = "data/fonts/Roboto-Regular.ttf"
         self.text = ""
         self.color = (0,0,0,1)
@@ -189,64 +266,73 @@ class Cell(Button):
 
     def display_one(self):
         """Displays the One"""
+        self.disabled = True
         self.font_name = "data/fonts/Roboto-Regular.ttf"
         self.text = "1"
-        self.color = one_color
+        self.disabled_color = one_color
         self.bold = True
         self.background_normal = "down.png"
 
     def display_two(self):
         """Displays the Two"""
+        self.disabled = True
         self.font_name = "data/fonts/Roboto-Regular.ttf"
         self.text = "2"
-        self.color = two_color
+        self.disabled_color = two_color
         self.background_normal = "down.png"
 
     def display_three(self):
         """Displays the Three"""
+        self.disabled = True
         self.font_name = "data/fonts/Roboto-Regular.ttf"
         self.text = "3"
-        self.color = three_color
+        self.disabled_color = three_color
         self.background_normal = "down.png"
 
     def display_four(self):
         """Displays the Four"""
+        self.disabled = True
         self.font_name = "data/fonts/Roboto-Regular.ttf"
         self.text = "4"
-        self.color = four_color
+        self.disabled_color = four_color
         self.background_normal = "down.png"
     
     def display_five(self):
         """Displays the Five"""
+        self.disabled = True
         self.font_name = "data/fonts/Roboto-Regular.ttf"
         self.text = "5"
-        self.color = five_color
+        self.disabled_color = five_color
         self.background_normal = "down.png"
 
     def display_six(self):
         """Displays the Six"""
+        self.disabled = True
         self.font_name = "data/fonts/Roboto-Regular.ttf"
         self.text = "6"
-        self.color = six_color
+        self.disabled_color = six_color
         self.background_normal = "down.png"
 
     def display_seven(self):
         """Displays the Seven"""
+        self.disabled = True
         self.font_name = "data/fonts/Roboto-Regular.ttf"
         self.text = "7"
-        self.color = seven_color
+        self.disabled_color = seven_color
         self.background_normal = "down.png"
 
     def display_eight(self):
         """Displays the Eight"""
+        self.disabled = True
         self.font_name = "data/fonts/Roboto-Regular.ttf"
         self.text = "8"
-        self.color = eight_color
+        self.disabled_color = eight_color
         self.background_normal = "down.png"
     
     def conceal(self):
         """Makes the Cell text blank"""
         self.display_zero()
+        self.disabled = False
         self.background_normal = "normal.png"
     
     def updateDisplay(self):
@@ -317,6 +403,8 @@ class MainMenu(BoxLayout):
         layout = BoxLayout(orientation = "vertical")
         game = Popup(title = "Game", content = layout)
 
+        tool_bar = ToolBar()
+        layout.add_widget(tool_bar)
         back = Button(text = "Back", size_hint_y = 0.1)
         back.bind(on_release = game.dismiss)
         back.background_normal = "normal.png"
@@ -324,7 +412,7 @@ class MainMenu(BoxLayout):
         back.color = (0,0,0,1)
         back.background_color = (.8,.8,.8,1)
 
-        layout.add_widget(GameBoard(width, height, bomb_count))
+        layout.add_widget(GameBoard(width, height, bomb_count, tool_bar))
         layout.add_widget(back)
 
         game.open()
